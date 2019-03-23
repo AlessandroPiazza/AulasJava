6ff66f68c1ac14f5e6c8cd2e55de7a8a6f0a0b31
Alessandro Piazza <alemaopiazza69@gmail.com>
2019-03-23 15:51:37 -0300

    Final da aula

---
+++ src/br/com/microlins/AtividadeImp.java
@@ -0,0 +1,13 @@
+package br.com.microlins;
+
+import br.com.microlins.interfaces.IAtividade;
+
+public class AtividadeImp implements IAtividade{
+
+	@Override
+	public String resumoAtividade() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/br/com/microlins/PessoaImp.java
@@ -0,0 +1,22 @@
+package br.com.microlins;
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.microlins.interfaces.IPessoa;
+
+public class PessoaImp implements IPessoa {
+	
+	private List<String> lista = new ArrayList<>();
+
+	@Override
+	public String exibirDados() {
+		return null;
+	}
+
+	@Override
+	public double calculaImc() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
