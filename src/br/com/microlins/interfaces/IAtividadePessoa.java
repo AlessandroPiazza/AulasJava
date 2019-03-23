cd0449ace055bf22f40301f81b2695af2408b949
Alessandro Piazza <alemaopiazza69@gmail.com>
2019-03-23 15:39:46 -0300

    Buffer

--- src/br/com/microlins/interfaces/IAtividadePessoa.java
+++ src/br/com/microlins/interfaces/IAtividadePessoa.java
@@ -7,20 +7,59 @@ import br.com.microlins.Pessoa;
 
 public interface IAtividadePessoa {
 
+	/**
+	 * Registra uma atividade
+	 * @param atividade
+	 * @return
+	 * 	Gravado com sucesso ou não (true or false)
+	 */
 	boolean gravarAtividade(Atividade atividade);
-
+	
+	/**
+	 * Recupera a lista de atividades da pessoa
+	 * @param pessoa
+	 * @return 
+	 * 	Retorna uma lista com todas as atividades feitas por uma pessoa
+	 */
 	List<Atividade> recuperarTodasAtividadesPorPessoa(Pessoa pessoa);
-
+	
+	/**
+	 * Recupera a atividade mais recente que a pessoa fez
+	 * @param pessoa
+	 * @return
+	 * 	Retorna a atividade
+	 */
 	Atividade recuperarUltimaAtividadePessoa(Pessoa pessoa);
-
+	
+	/**
+	 * Remove uma atividade feita
+	 * @param atividade
+	 * @return
+	 */
 	boolean removerAtividade(Atividade atividade);
-
+	
+	/**
+	 * Retorna a quantidade de atividades que uma pessoa fez
+	 * @param pessoa
+	 * @return
+	 */
 	int quantidadeAtividadesRealizadasPorPessoa(Pessoa pessoa);
-
+	
+	/**
+	 * Retorna a quantidade de todas as atividades feitas no sistema
+	 * @return
+	 */
 	int totalAtividades();
-
+	
+	/**
+	 * Limpa a lista
+	 */
 	void limparLista();
-
+	
+	/**
+	 * Consulta a ultima atividade inserida
+	 * @return
+	 */
 	Atividade consultarUltimaAtividadeRealizada();
-
+	
 }
